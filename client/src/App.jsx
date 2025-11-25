bbf67cb99ccc75d95c3b7efad6e10edf05696cee
Amandev25 <amanyadav.dev25@gmail.com>
2025-11-25 21:45:23 +0530

    Issue Checking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const {authUser} = useContext(AuthContext) ; 
 
   return (
-    <div className= "min-h-screen bg-[#0b0b10] bg-[url('/bgImage.svg')] bg-cover bg-center" >
+    <div className= "bg-[url('/bgImage.svg')] bg-contain" >
       <Toaster/>
       <Routes>
         <Route path='/' element ={authUser ? <HomePage/> : <Navigate to="/login"/>} /> // agr user login : homepage else : login page 
